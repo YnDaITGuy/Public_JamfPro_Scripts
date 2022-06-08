fa3175907240301a33c95036733745d8e00ba715
YnDaITGuy <92037248+YnDaITGuy@users.noreply.github.com>
2022-06-08 11:02:34 -0400

    Update Lock The Dock.sh

--- Lock The Dock.sh
+++ Lock The Dock.sh
@@ -2,6 +2,7 @@
 
 currentUser=$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ { print $3 }' )
 
+## true = lock, false = unlock
 sudo -u "$currentUser" defaults write com.apple.Dock contents-immutable -bool true
 
 killall Dock
