350ccf3b3dcbebc09cd033fe3dd61b56c8dc7113
YnDaITGuy <92037248+YnDaITGuy@users.noreply.github.com>
2022-12-15 09:31:44 -0500

    Update Control Center Icon.sh

--- Control Center Icon.sh
+++ Control Center Icon.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-loggedInUser=$( ls -l /dev/console | awk '{print $3}' )
+currentUser=$(ls -l /dev/console | awk '{print $3}')
 
 ## Big Sur and above
 
 ## Display Bluetooth on Menu Bar
-sudo -u ${loggedInUser} defaults write /Users/${loggedInUser}/Library/Preferences/ByHost/com.apple.controlcenter.plist Bluetooth -int 18
+sudo -u ${currentUser} defaults write /Users/${loggedInUser}/Library/Preferences/ByHost/com.apple.controlcenter.plist Bluetooth -int 18
 
 ## Display Sound on Menu Bar
-sudo -u ${loggedInUser} defaults write /Users/${loggedInUser}/Library/Preferences/ByHost/com.apple.controlcenter.plist Sound -int 18
+sudo -u ${currentUser} defaults write /Users/${loggedInUser}/Library/Preferences/ByHost/com.apple.controlcenter.plist Sound -int 18
 
 ## Display Battery Precentage on Menu Bar
-sudo -u ${loggedInUser} defaults write /Users/${loggedInUser}/Library/Preferences/ByHost/com.apple.controlcenter.plist BatteryShowPercentage -bool true
+sudo -u ${currentUser} defaults write /Users/${loggedInUser}/Library/Preferences/ByHost/com.apple.controlcenter.plist BatteryShowPercentage -bool true
 
 exit 0
