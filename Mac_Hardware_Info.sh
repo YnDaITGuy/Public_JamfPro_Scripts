35a0450f09ce98a3f39d8673a2e335b80d995271
YnDaITGuy <92037248+YnDaITGuy@users.noreply.github.com>
2022-12-15 07:55:03 -0500

    Update Mac_Hardware_Info.sh

--- Mac_Hardware_Info.sh
+++ Mac_Hardware_Info.sh
@@ -4,3 +4,6 @@
 
 modelID=$(system_profiler SPHardwareDataType | awk '/Name/ {print $3}')
 echo "Model ID: $modelID"
+
+serialNumber=$(system_profiler SPHardwareDataType | awk '/Serial/ {print $4}')
+echo "$serialNumber"
